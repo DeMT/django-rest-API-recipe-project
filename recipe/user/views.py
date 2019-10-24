f7fb4f201c0cc25d83167fee895b233809ff9e88
DeMT <gn00468461@gmail.com>
2019-10-24 19:16:47 +0000

    added end points for creating and authenticating user.

---
+++ recipe/user/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+
+class CreateUserviews(generics.CreateAPIView):
+    """Create a new user in the system via api"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new token view for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
