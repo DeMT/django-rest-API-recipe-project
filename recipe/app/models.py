58b21d3b12834af8a3e187fa203ddf75f96070ab
DeMT <gn00468461@gmail.com>
2019-12-10 19:13:25 +0000

    added recipe model.

--- recipe/app/models.py
+++ recipe/app/models.py
@@ -51,3 +51,17 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE)
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    Ingredient = models.ManyToManyField('Ingredient')
+    tag = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
